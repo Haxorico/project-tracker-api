790b0e7af1e6224c45aaa8688ddb15f01a412a11
Haxorico <haxorico@gmail.com>
2020-09-25 12:53:02 +0300

    #Update 3 Add Projects API
    Added basic projects API: GetID, GetAll, CreateNew, Update, Delete

---
+++ server/routes/projects.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const COLLECTION_NAME = 'projects';
+/* GET tasks listing. */
+
+router.get('/', async (req, res, next) => {
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(COLLECTION_NAME);
+  collection.find({}).toArray((error, projects) => {
+    res.json(projects);
+  });
+});
+
+router.get('/:id', function (req, res) {
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(COLLECTION_NAME);
+  collection.find({ id: req.params.id }).next().then(project => {
+    res.json(project);
+  });
+});
+
+/* POST task. */
+router.post('/', async (req, res, next) => {
+  const db = require('../libs/mongodb').getDb();
+  db.collection(COLLECTION_NAME).insertOne(req.body)
+});
+
+/* PUT (update) task. */
+router.put('/:id', async (req, res, next) => {
+  const db = require('../libs/mongodb').getDb();
+  db.collection(COLLECTION_NAME).updateOne({ id: req.params.id }, { $set: req.body });
+});
+
+router.delete('/:id', function (req, res) {
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(COLLECTION_NAME);
+  collection.deleteOne({ id: req.params.id }).then(project => {
+    res.end();
+  }).catch(err => {  
+    console.log(err);
+  });
+})
+
+
+module.exports = router;
