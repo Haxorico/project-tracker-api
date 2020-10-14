cfab4d19da11a6f138829a6d0b096a02f8d99684
Haxorico <haxorico@gmail.com>
2020-10-14 22:01:52 +0300

    Update #5 Adding a user-service
    Added a user-service to take care of all the get/send data. Making the route files cleaner.
    
    #TODO: Add the services for all entities. Waiting on Alex's approval

--- server/services/users.js
+++ server/services/users.js
@@ -1,19 +1,46 @@
-const express = require('express');
-const router = express.Router();
+const ent_name = 'users';
 
+//#ASK_ALEX: If I want to sort users by their rank. Should I add a function here or should the service only handle getting and updating data?
 
-async function GetUsers(obj) {
-//#ASK_ALEX Should I declare db and collection every time?
-    //If I ust have them as global variables, will they be updated WITH the DB?
-    const db = require('../libs/mongodb').getDb();
-  const collection = db.collection('users');
-  console.log("GetUsers is running");
-  //#ASK_ALEX Should I return the data as raw and let the route translate it to JSON if needed?
-  collection.find(obj).toArray((error, users) => {
-    console.log("Returning Data"); 
-    resolve(users);
-    //return Promise.resolve(users);
+function GetUsers(query_paramters) {
+  //#ASK_ALEX Should I declare db and collection every time?
+  //If I ust have them as global variables, will they be updated WITH the DB?
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(ent_name);
+  return new Promise((resolve, reject) => {
+    collection.find(query_paramters).toArray((error, users) => {
+      resolve(users);
+    });
   });
 }
 
-module.exports = { GetUsers }
+function GetUser(query_paramters){
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(ent_name);
+  return new Promise((resolve, reject) => {
+    collection.find(query_paramters).next().then(user => {
+      resolve(user);
+    });
+  });
+}
+
+function AddUser(user_to_add){
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(ent_name);
+  console.log(user_to_add);
+  collection.insertOne(user_to_add);
+}
+
+function UpdateUser(user_to_update){
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(ent_name);
+  collection.updateOne({ id: user_to_update.id }, user_to_update);
+}
+
+function DeleteUser(user_to_delete){
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection(ent_name);
+  collection.deleteOne({ id: user_to_delete.id });
+}
+
+module.exports = { GetUsers, GetUser, AddUser, UpdateUser, DeleteUser }
