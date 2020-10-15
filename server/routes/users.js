2e7cbbd73f7cee003c7f92a98f770755b52f7ff2
Haxorico <haxorico@gmail.com>
2020-10-15 10:00:10 +0300

    Update #6 - Add Login Feature
    Added the login feature to BE.

--- server/routes/users.js
+++ server/routes/users.js
@@ -19,13 +19,38 @@ router.get('/:id', async (req, res, next) => {
   res.json(data);
 });
 
+
 router.post('/', async (req, res, next) => {
   UserService.AddUser(req.body);
+});
+
+router.post('/:login', async (req, res, next) => {
+  //#NOTES_FOR_ALEX:
+  //this entire thing will ofcourse be moved into the services.
+  //This is just a prototype to understand from you if it is okay and should I build upon it.
+  //Also at the moment I only return true or false. If all is well I will proceed with the token.
+  const db = require('../libs/mongodb').getDb();
+  const collection = db.collection('users');
+  const user_to_find = req.body;
+  
+  console.log("#############");
+  collection.find({name: user_to_find.name}).next().then(user => {
+    console.log("user found");
+    if (user.password == user_to_find.password){
+      console.log("passwords match");
+      res.json(true);
+    }
+    else{
+      console.log("passwords do not match");
+      res.json(false);
+    }
+  });
 });
 
 router.put('/:id', async (req, res, next) => {
   UserService.UpdateUser(req.body);
 });
+
 
 router.delete('/:id', function (req, res) {
   UserService.DeleteUser(req.body);
